ab7d018cb4583e9c9d2c951453b40ef57a8a1eb9
roh210 <roheena.s786@gmail.com>
2023-05-15 13:02:37 +0100

    first commit

--- index.js
+++ index.js
@@ -4,4 +4,26 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove("menu--open");
 }
-)
+
+///
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("testimonial__slideshow__wrapper");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  
+}
